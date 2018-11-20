624ffbad79b8af4d2e3507955d4c5e365b7d845c
shreelimbkar <limbkarshree@gmail.com>
2018-11-20 16:20:17 +0530

    removed blog content
    removed blog content

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,22 @@ import Link from 'next/link';
 import Layout from '../comps/MyLayout';
 import fetch from 'isomorphic-unfetch';
 
-const PostLink = (props) => (
-  <li>
-      <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-          <a>{props.title}</a>
-      </Link>
-  </li>
-)
+// const PostLink = (props) => (
+//   <li>
+//       <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+//           <a>{props.title}</a>
+//       </Link>
+//   </li>
+// )
 
 const Index = (props) => (
   <Layout>
-    <h1>My Blog</h1>
+    {/* <h1>My Blog</h1>
     <ul>
       <PostLink id="hello-nextjs" title="Hello Next.js" />
       <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
       <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
-    </ul>
+    </ul> */}
 
     <h1>Batman TV Shows</h1>
     <ul>

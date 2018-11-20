0cb5a157902fa6088cc5057998d48e37d603e14e
shreelimbkar <limbkarshree@gmail.com>
2018-11-20 16:20:58 +0530

    added style for link
    added style for link

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,22 @@ const Index = (props) => (
             <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
               <a>{show.name}</a>
             </Link>
+            <style jsx>{`
+              li {
+                list-style: none;
+                margin: 5px 0;
+              }
+
+              a {
+                text-decoration: none;
+                color: blue;
+                font-family: "Arial";
+              }
+
+              a:hover {
+                opacity: 0.6;
+              }
+            `}</style>
           </li>
         )
       })}
